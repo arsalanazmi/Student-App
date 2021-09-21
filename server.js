4cbf94b461bfe28735deb0598b33cf63b5778d29
arsalan raza azmi <arsalan.azmi92@gmail.com>
2021-09-22 01:11:22 +0500

    updated

--- server.js
+++ server.js
@@ -19,6 +19,9 @@ app.use("/students", studentRoute);
 
 if (process.env.NODE_ENV === "production"){
   app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+  });
 }
 
 app.listen(PORT, () => {
